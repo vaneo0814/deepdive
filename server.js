460909865ae7f29199543135bbd74a0b6c89b667
Vanessa Orellana <vorellana814@gmail.com>
2021-01-05 18:29:12 -0500

    mailchimp working

--- server.js
+++ server.js
@@ -1,77 +1,72 @@
 const express = require("express");
-const path = require("path");
+const request = require("request");
 const bodyParser = require("body-parser");
-const { request } = require("https");
-var http = require('https');
+const path = require("path");
+
 
 const app = express();
-const PORT = process.env.PORT || 8000;
+const PORT = process.env.PORT || 8080;
 
-//Bodyparser Middleware
-app.use(bodyParser.urlencoded({extended: true}));
-
+//Middleware
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, 'public')));
+//Body parser middleware
+app.use(bodyParser.urlencoded({ extended: false }));
 
-//subscribe route
-app.post('/subscribe', (req,res) => {
-const {email} = req.body;
+app.post('/subscribe', (req, res) => {
+  console.log("yo!");
+  const { email, js} = req.body;
 
-if(!email) {
-  res.redirect("/index.html");
-  return;
-}
-
-//Construct req data
-const data = {
-  members: [
-    {
-      email_address: email,
-      status: 'subscribed'
-    }
-  ]
-};
-const postData = JSON.stringify(data);
-
-const options = {
-  url: 'https://us7.api.mailchimp.com/3.0/lists/dde4d2d85d',
-  path: '/3.0/',
-  method: 'POST',
-  headers: {
-    Authorization: 'auth 9608dee63ad924359446c464b6f948ab-us7',
-    'content-type': 'application/json'
-  },
-  body: postData
-};
-
-http.get(options, (res) => {
-  console.log(`Got response: ${res.statusCode}`);
-  res.on('data', (chunk) => {
-    console.log(`BODY: ${chunk}`);
-  });
-  // consume response body
-  res.resume();
-}).on('uncaughtException', function (err) { console.log(err); });
-
-
-
-//is it not grabbing body?
-console.log("This is the data grabbing: " + options.body);
-request(options, (err, response, body) => {
-  if(err) {
-  res.redirect('/index.html');
-  }else {
-    if(response.statusCode === 200){
-      res.redirect('/subscribe')
-    } else {
-      res.redirect('/index.html')
-    }
+  const mcData = {
+    members: [
+      {
+        email_address: email,
+        status: 'subscribed'
+      }
+    ]
   }
 
-});
+  const mcDataPost = JSON.stringify(mcData);
+
+  const options = {
+    url: 'https://us7.api.mailchimp.com/3.0/lists/dde4d2d85d',
+    method: 'POST',
+    headers: {
+      Authorization: 'auth 9608dee63ad924359446c464b6f948ab-us7'
+    },
+    body: mcDataPost
+  }
+
+  if(email){
+    request (options, (err, response, body) => {
+      if(err) {
+        res.json({error: err})
+      }else {
+        if(js) {
+          res.sendStatus(200);
+      } else {
+        res.redirect('/index.html');
+      }
+    }
+    })
+  }else{
+    res.status(404).send({message: 'Failed to subscribe'});
+  }
 });
 
-app.listen(PORT, function() {
-    console.log("App listening on PORT: " + PORT);
-  });
+
+
+
+
+
+
+
+
+
+
+
+app.listen(PORT, function () {
+  console.log("App listening on PORT: " + PORT);
+});
